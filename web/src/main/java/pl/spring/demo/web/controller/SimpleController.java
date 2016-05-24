a9a7288f5910720a5cb1b0d979d3d848a7f612fe
TomaszKwolek <tomasz.kwolek@capgemini.com>
2016-05-24 12:35:16 +0200

    init

---
+++ web/src/main/java/pl/spring/demo/web/controller/SimpleController.java
@@ -0,0 +1,22 @@
+package pl.spring.demo.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import pl.spring.demo.to.BookTo;
+import pl.spring.demo.to.LibraryTo;
+
+import java.awt.print.Book;
+import java.util.Map;
+
+@Controller
+public class SimpleController {
+
+    @RequestMapping(value = "/home", method = RequestMethod.GET)
+    public String home(Map<String, Object> parameters) {
+        parameters.put("book", new BookTo(1L, "First Book Title", null, "First Library"));
+        parameters.put("booksCount", 1);
+        return "home";
+    }
+}
+
